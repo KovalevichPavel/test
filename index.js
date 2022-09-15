a25124f61432ad21133ffed733b46c8cd973be47
KovalevichPavel <kovalevichpasha1@gmail.com>
2022-09-15 23:06:11 +0300

    commited in new branch and add interection to user

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 alert('js file was created');
+
+function showAnswer() {
+    let ans = prompt('how r u?');
+    const div = document.createElement('div');
+    div.innerHTML = `your answer is ${ans}`;
+    document.body.append(div) 
+}
